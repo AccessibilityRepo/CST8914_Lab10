17b156d90ca8fe0840c0f157937f238c45e15638
AccessibilityRepo <tyao60357@gmail.com>
2024-11-08 18:01:48 -0500

    Initial commit

---
+++ .history/JS_20241108175645.js
@@ -0,0 +1,255 @@
+/*
+ *   This content is licensed according to the W3C Software License at
+ *   https://www.w3.org/Consortium/Legal/2015/copyright-software-and-document
+ *   Desc: Menu button that opens a menu of actions.
+ */
+
+'use strict';
+
+class MenuButtonActions {
+  constructor(domNode, performMenuAction) {
+    this.domNode = domNode;
+    this.performMenuAction = performMenuAction;
+    this.buttonNode = domNode.querySelector('button');
+    this.menuNode = domNode.querySelector('[role="menu"]');
+    this.menuitemNodes = [];
+    this.firstMenuitem = false;
+    this.lastMenuitem = false;
+    this.firstChars = [];
+
+    // Add event listeners for button interactions
+    this.buttonNode.addEventListener(
+      'keydown',
+      this.onButtonKeydown.bind(this)
+    );
+    this.buttonNode.addEventListener('click', this.onButtonClick.bind(this));
+
+    // Query and iterate over menu items, setting up event listeners
+    var nodes = domNode.querySelectorAll('[role="menuitem"]');
+
+    for (var i = 0; i < nodes.length; i++) {
+      var menuitem = nodes[i];
+      this.menuitemNodes.push(menuitem);
+      menuitem.tabIndex = -1; // Changed: Initialize tabindex for menu items to -1 (unfocusable)
+      this.firstChars.push(menuitem.textContent.trim()[0].toLowerCase());
+
+      menuitem.addEventListener('keydown', this.onMenuitemKeydown.bind(this));
+      menuitem.addEventListener('click', this.onMenuitemClick.bind(this));
+      menuitem.addEventListener(
+        'mouseover',
+        this.onMenuitemMouseover.bind(this)
+      );
+
+      if (!this.firstMenuitem) {
+        this.firstMenuitem = menuitem;
+      }
+      this.lastMenuitem = menuitem;
+    }
+
+    // Add focus in and focus out event listeners for handling focus styles
+    domNode.addEventListener('focusin', this.onFocusin.bind(this));
+    domNode.addEventListener('focusout', this.onFocusout.bind(this));
+
+    // Add mousedown event listener on window to handle clicks outside the menu
+    window.addEventListener(
+      'mousedown',
+      this.onBackgroundMousedown.bind(this),
+      true
+    );
+  }
+
+  // Changed: Added logic to handle roving tabindex
+  setFocusToMenuitem(newMenuitem) {
+    this.menuitemNodes.forEach((item) => {
+      item.tabIndex = -1; // Make all items unfocusable
+    });
+    newMenuitem.tabIndex = 0; // Make the new focused item focusable
+    newMenuitem.focus(); // Move focus to the new item
+  }
+
+  setFocusToFirstMenuitem() {
+    this.setFocusToMenuitem(this.firstMenuitem);
+  }
+
+  setFocusToLastMenuitem() {
+    this.setFocusToMenuitem(this.lastMenuitem);
+  }
+
+  setFocusToPreviousMenuitem(currentMenuitem) {
+    var newMenuitem, index;
+
+    if (currentMenuitem === this.firstMenuitem) {
+      newMenuitem = this.lastMenuitem;
+    } else {
+      index = this.menuitemNodes.indexOf(currentMenuitem);
+      newMenuitem = this.menuitemNodes[index - 1];
+    }
+
+    this.setFocusToMenuitem(newMenuitem);
+  }
+
+  setFocusToNextMenuitem(currentMenuitem) {
+    var newMenuitem, index;
+
+    if (currentMenuitem === this.lastMenuitem) {
+      newMenuitem = this.firstMenuitem;
+    } else {
+      index = this.menuitemNodes.indexOf(currentMenuitem);
+      newMenuitem = this.menuitemNodes[index + 1];
+    }
+
+    this.setFocusToMenuitem(newMenuitem);
+  }
+
+  // Popup menu methods
+  openPopup() {
+    this.menuNode.style.display = 'block';
+    this.buttonNode.setAttribute('aria-expanded', 'true');
+    this.setFocusToFirstMenuitem(); // Changed: Set focus to the first item when menu opens
+  }
+
+  closePopup() {
+    if (this.isOpen()) {
+      this.buttonNode.removeAttribute('aria-expanded');
+      this.menuNode.style.display = 'none';
+    }
+  }
+
+  isOpen() {
+    return this.buttonNode.getAttribute('aria-expanded') === 'true';
+  }
+
+  // Menu event handlers
+
+  onFocusin() {
+    this.domNode.classList.add('focus');
+  }
+
+  onFocusout() {
+    this.domNode.classList.remove('focus');
+  }
+
+  onButtonKeydown(event) {
+    var key = event.key,
+      flag = false;
+
+    switch (key) {
+      case ' ':
+      case 'Enter':
+      case 'ArrowDown':
+        this.openPopup();
+        this.setFocusToFirstMenuitem();
+        flag = true;
+        break;
+
+      case 'Escape':
+        this.closePopup();
+        flag = true;
+        break;
+
+      case 'ArrowUp':
+        this.openPopup();
+        this.setFocusToLastMenuitem();
+        flag = true;
+        break;
+
+      default:
+        break;
+    }
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  onButtonClick(event) {
+    if (this.isOpen()) {
+      this.closePopup();
+      this.buttonNode.focus();
+    } else {
+      this.openPopup();
+    }
+
+    event.stopPropagation();
+    event.preventDefault();
+  }
+
+  onMenuitemKeydown(event) {
+    var tgt = event.currentTarget,
+      key = event.key,
+      flag = false;
+
+    if (event.ctrlKey || event.altKey || event.metaKey) {
+      return;
+    }
+
+    switch (key) {
+      case 'Enter':
+        this.closePopup();
+        this.performMenuAction(tgt);
+        this.buttonNode.focus();
+        flag = true;
+        break;
+
+      case 'Escape':
+        this.closePopup();
+        this.buttonNode.focus();
+        flag = true;
+        break;
+
+      case 'ArrowUp':
+        this.setFocusToPreviousMenuitem(tgt);
+        flag = true;
+        break;
+
+      case 'ArrowDown':
+        this.setFocusToNextMenuitem(tgt);
+        flag = true;
+        break;
+
+      default:
+        break;
+    }
+
+    if (flag) {
+      event.stopPropagation();
+      event.preventDefault();
+    }
+  }
+
+  onMenuitemClick(event) {
+    var tgt = event.currentTarget;
+    this.closePopup();
+    this.buttonNode.focus();
+    this.performMenuAction(tgt);
+  }
+
+  onMenuitemMouseover(event) {
+    var tgt = event.currentTarget;
+    tgt.focus();
+  }
+
+  onBackgroundMousedown(event) {
+    if (!this.domNode.contains(event.target)) {
+      if (this.isOpen()) {
+        this.closePopup();
+        this.buttonNode.focus();
+      }
+    }
+  }
+}
+
+// Initialize menu buttons
+window.addEventListener('load', function () {
+  document.getElementById('action_output').value = 'none';
+
+  function performMenuAction(node) {
+    document.getElementById('action_output').value = node.textContent.trim();
+  }
+
+  var menuButtons = document.querySelectorAll('.menu-button-actions');
+  for (var i = 0; i < menuButtons.length; i++) {
+    new MenuButtonActions(menuButtons[i], performMenuAction);
+  }
+});
